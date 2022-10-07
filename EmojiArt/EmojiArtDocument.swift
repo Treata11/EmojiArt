6c46a099bdc0bf825f1df922a3127b993a685f81
wempaus <58611349+wempaus@users.noreply.github.com>
2022-10-07 16:03:25 +0330

    Add EmojiArtDocument as VM, Rename View to EmojiArtDocumentView

---
+++ EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,54 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by Treata Norouzi on 10/7/22.
+//
+
+import SwiftUI
+
+
+class EmojiArtDocument: ObservableObject {
+    @Published private(set) var emojiArt: EmojiArtModel
+    
+    init() {
+        emojiArt = EmojiArtModel()
+    }
+    
+    var emojis: [EmojiArtModel.Emoji] { emojiArt.emojis }
+    var background: EmojiArtModel.Background { emojiArt.background }
+    
+    // MARK: - Intent(s)
+    
+    func setBackground(_ background: EmojiArtModel.Background) {
+        return emojiArt.background = background
+    }
+    
+    func addEmoji(_ emoji: String, at location: (x: Int, y: Int), size: CGFloat) {
+        emojiArt.addEmoji(emoji, at: location, size: Int(size))
+    }
+    
+    func moveEmoji(_ emoji: EmojiArtModel.Emoji, by offset: CGSize) {
+        if let index = emojiArt.emojis.index(matching: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.height)
+        }
+    }
+    
+    func scaleEmoji(_ emoji: EmojiArtModel.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.index(matching: emoji) {
+            emojiArt.emojis[index].size = Int(CGFloat(emojiArt.emojis[index].size) * scale.rounded(.toNearestOrAwayFromZero))
+            
+        }
+    }
+}
+
+// MARK: - Extension(s)
+
+extension Collection where Element: Identifiable {
+    func index(matching element: Element) -> Self.Index? {
+        firstIndex(where: {$0.id == element.id} )
+    }
+}
+
+
