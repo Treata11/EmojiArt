1222254509692b081b35d874173dc476e07bcf2c
Treata11 <treata11@yahoo.com>
2022-11-03 19:26:09 +0330

    Add PalleteStore MVVM

---
+++ EmojiArt/PalleteStore.swift
@@ -0,0 +1,24 @@
+//
+//  PalleteStore.swift
+//  EmojiArt
+//
+//  Created by Treata Norouzi on 11/3/22.
+//
+
+import SwiftUI
+
+struct Pallete: Identifiable {
+    var name: String
+    var emojis: String
+    var id: Int
+}
+
+class PalleteStore {
+    let name: String
+    
+    @Published var pallete = [Pallete]()
+    
+    init(named name: String) {
+        self.name = name
+    }
+}
