fb2da1f08df735b365c22f2905af0bf6ac1152c1
Treata11 <treata11@yahoo.com>
2022-11-30 16:31:41 +0330

    Add addEmojis function to PaletteStore

--- EmojiArt/PaletteEditor.swift
+++ EmojiArt/PaletteEditor.swift
@@ -20,6 +20,7 @@ struct PaletteEditor: View {
     var nameSection: some View {
         Section(header: Text("Name")) {
             TextField("Name", text: $palette.name)
+                .disableAutocorrection(true)
         }
     }
     
@@ -28,9 +29,20 @@ struct PaletteEditor: View {
     var addEmojisSection: some View {
         Section(header: Text("Add Emojis")) {
             TextField("", text: $emojisToAdd)
+                .onChange(of: emojisToAdd) { emojis in
+                    addEmojis(emojis)
+                }
         }
     }
-}
+    
+    func addEmojis(_ emojis: String) {
+        withAnimation {
+            palette.emojis = (emojis + palette.emojis)
+                .filter { $0.isEmoji }
+                .removingDuplicateCharacters
+        }
+    }
+ }
 
 struct PaletteEditor_Previews: PreviewProvider {
     static var previews: some View {
