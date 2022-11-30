a85028ce3780e883f0f6cb9ec8f79e3a1ecc9636
Treata11 <treata11@yahoo.com>
2022-11-30 16:12:43 +0330

    Add nameSection View to PaletteEditor

--- EmojiArt/PaletteEditor.swift
+++ EmojiArt/PaletteEditor.swift
@@ -12,15 +12,30 @@ struct PaletteEditor: View {
     
     var body: some View {
         Form {
-            TextField("Name", text: $palette.name)
+            nameSection
         }
         .frame(minWidth: 333, minHeight: 539)
+    }
+    
+    var nameSection: some View {
+        Section(header: Text("Name")) {
+            TextField("Name", text: $palette.name)
+        }
+    }
+    
+    @State private var emojisToAdd = ""
+    
+    var addEmojisSection: some View {
+        Section(header: Text("Add Emojis")) {
+            TextField("", text: <#T##Binding<String>#>)
+        }
     }
 }
 
 struct PaletteEditor_Previews: PreviewProvider {
     static var previews: some View {
         Text("Fix me!")
+            .previewLayout(.fixed(width: 300, height: 400))
 //        PaletteEditor()
     }
 }
