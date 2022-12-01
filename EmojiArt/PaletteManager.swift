f67a81ff078a384112bdf56ab81da3bb62c9e69b
Treata11 <treata11@yahoo.com>
2022-12-01 14:40:31 +0330

    Implement EditMode inside PaletteManager List View

--- EmojiArt/PaletteManager.swift
+++ EmojiArt/PaletteManager.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct PaletteManager: View {
     @EnvironmentObject var store: PaletteStore
+    @Environment(\.colorScheme) var colorScheme
+    
+    @State var editMode: EditMode = .inactive
     
     var body: some View {
         NavigationView {
@@ -16,7 +19,7 @@ struct PaletteManager: View {
                 ForEach(store.palettes) { palette in
                     NavigationLink(destination: PaletteEditor(palette: $store.palettes[palette])) {
                         VStack(alignment: .leading) {
-                            Text(palette.name)
+                            Text(palette.name).font(editMode == .active ? .largeTitle : .subheadline)
                             Text(palette.emojis)
                         }
                     }
@@ -24,6 +27,10 @@ struct PaletteManager: View {
             }
             .navigationTitle("Manage Palettes")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                EditButton()    // Toggles the value of editMode in its environment
+            }
+            .environment(\.editMode, $editMode) // A binding EnvironmentValue
         }
     }
 }
@@ -31,6 +38,7 @@ struct PaletteManager: View {
 struct PaletteManager_Previews: PreviewProvider {
     static var previews: some View {
         PaletteManager()
+            .previewDevice("iP hone 14")
             .environmentObject(PaletteStore(named: "Preview"))
     }
 }
