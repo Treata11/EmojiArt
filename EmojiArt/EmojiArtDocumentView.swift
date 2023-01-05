9ea6da0001b2d36f019070a14193774909b34daa
Treata11 <treata11@yahoo.com>
2023-01-06 00:42:21 +0330

    Implement undo, redo, paste background background in EmojiArtDocument.swift's toolbar

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -63,12 +63,31 @@ struct EmojiArtDocumentView: View {
                 }
             }
             .toolbar {
-                UndoButton(
-                    undo: undoManager?.optionalUndoMenuItemTitle,
-                    redo: undoManager?.optionalRedoMenuItemTitle
-                )
+                ToolbarItemGroup(placement: .bottomBar) {
+                    AnimatedActionButton(title: "Paste Background", systemImage: "doc.on.clipboard") {
+                        pasteBackground()
+                    }
+                    if let undoManager = undoManager {
+                        if undoManager.canUndo {
+                            AnimatedActionButton(title: undoManager.undoActionName, systemImage: "arrow.uturn.backward") {
+                                undoManager.undo()
+                            }
+                        }
+                    }
+                    if let undoManager = undoManager {
+                        if undoManager.canRedo {
+                            AnimatedActionButton(title: undoManager.redoActionName, systemImage: "arrow.uturn.forward") {
+                                undoManager.redo()
+                            }
+                        }
+                    }
+                }
             }
         }
+    }
+    
+    private func pasteBackground() {
+        
     }
     
     @State private var autozoom = false
