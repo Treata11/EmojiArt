7bcd2c52501f5f0c86ed2875233b0eed0f7a3189
Treata11 <treata11@yahoo.com>
2023-01-14 10:48:06 +0330

    Implement PhotoLibrary.swift from the OldWorld

--- EmojiArt/PhotoLibrary.swift
+++ EmojiArt/PhotoLibrary.swift
@@ -9,19 +9,42 @@ import SwiftUI
 import PhotosUI
 
 struct photoLibrary: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> PHPickerViewController {
-        <#code#>
-    }
-    
-    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
-        <#code#>
-    }
-    
-    typealias UIViewControllerType = PHPickerViewController
-    
     var handlePickedImage: (UIImage?) -> Void
     
     var isAvailable: Bool {
         return true
+    }
+    
+    func makeCoordinator() -> Coordinator {
+            Coordinator(handlePickedImage: handlePickedImage)
+    }
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        // nothing to do
+    }
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var handlePickedImage: (UIImage?) -> Void
+        
+        init(handlePickedImage: @escaping (UIImage?) -> Void) {
+            self.handlePickedImage = handlePickedImage
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            let found = results.map { $0.itemProvider }.loadObjects(ofType: UIImage.self) { [weak self] image in
+                self?.handlePickedImage(image)
+            }
+            if !found {
+                handlePickedImage(nil)
+            }
+        }
     }
 }
